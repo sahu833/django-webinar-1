e1b68e51dfed354fb1056b4ed5af1b83b0b33e21
Anshul Ahuja <anshul.ahu@gmail.com>
2020-03-29 22:43:53 +0530

    Add complain app

---
+++ mysite/complain/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create your models here.
+
+class Complain(models.Model):
+    student_name = models.CharField(max_length=128, null=True, blank=True)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.title
