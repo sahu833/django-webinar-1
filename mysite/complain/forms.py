e4ff1e8ec6db2d649b8c306fbf299c3c614affd7
Anshul Ahuja <anshul.ahu@gmail.com>
2020-03-29 23:14:07 +0530

    Create form

---
+++ mysite/complain/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Complain
+
+
+class CreateComplainForm(forms.ModelForm):
+    class Meta:
+        model = Complain
+        fields = '__all__'
+        # fields = [ 'title', 'description', 'date']
--- mysite/complain/views.py
+++ mysite/complain/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .forms import CreateComplainForm
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+# function views
+def create_complain(request):
+    new_complain = CreateComplainForm
+    if request.method == 'POST':
+        new_complain = CreateComplainForm(request.POST)
+        if new_complain.is_valid():
+            new_complain.save()
+            return HttpResponse("Complain registered")
+        else:
+            return HttpResponse("Incorrect Format")
+
+    return render(request, 'complain/create-complain.html', {'form': CreateComplainForm})
