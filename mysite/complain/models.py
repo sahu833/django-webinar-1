eaaed604ff341ad0cbc464046d6920bb996f4dd5
sahu833 <sahu.9@iitj.ac.in>
2020-04-10 22:48:49 +0530

    Create form

--- mysite/complain/models.py
+++ mysite/complain/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Complain(models.Model):
-    student_name = models.Charfield(max_length=128, null=True, blank=True)
-    title = models.Charfield(max_length=64)
-    description = models.Textfield()
-    date= models.Textfield()
+    student_name = models.CharField(max_length=128, null=True, blank=True)
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    date = models.DateField()
 
 
     def __str__(self):
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from hello.views import hello_world
+from complain.views import create_complain
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello_world)
+    path('hello/', hello_world),
+    path('complain/', create_complain),
 ]
